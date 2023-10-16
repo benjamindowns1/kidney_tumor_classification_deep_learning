e92d5a87963a41450b1f0f9bb32fefe1c94eb125
Yashwant <yashwant140@outlook.com>
2023-10-16 19:17:51 +0530

    main

--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 # Kidney Disease Classifier using CNN# Kidney-disease-mlflow
 # Kidney-disease-mlflow_dagshub
+
+
+MLFLOW_TRACKING_URI=https://dagshub.com/juanelnino759/kidney_tumor_classification_deep_learning.mlflow \
+MLFLOW_TRACKING_USERNAME=juanelnino759 \
+MLFLOW_TRACKING_PASSWORD=0b47cff15dcb47249e96c2e834ef85ba999a3812 \
+python script.py
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from src.cnnClassifier.constants import *
 import os
-from src.cnnClassifier.utils.common import read_yaml,create_directories
-from src.cnnClassifier.entity.config_entity import DataIngestionConfig,PreparedBaseModelConfig,TrainingConfig
+from src.cnnClassifier.utils.common import read_yaml,create_directories,save_json
+from src.cnnClassifier.entity.config_entity import DataIngestionConfig,PreparedBaseModelConfig,TrainingConfig,EvaluationConfig
 import tensorflow as tf
 
 class ConfigurationManager:
@@ -69,4 +69,15 @@ class ConfigurationManager:
 
         return training_config
     
+    def get_evaluation_config(self)-> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model= r"artifacts\training\model.h5",
+            training_data= r"artifacts\data_ingestion\Data",
+            mlflow_uri= "https://dagshub.com/juanelnino759/kidney_tumor_classification_deep_learning.mlflow",
+            all_params= self.params,
+            params_image_size= self.params.IMAGE_SIZE,
+            params_batch_size= self.params.BATCH_SIZE
+        )
+
+        return eval_config
     
